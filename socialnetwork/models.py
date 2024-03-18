dd545b405f2ecaed23a80ea876917970abf4e589
Mariana <marianar@andrew.cmu.edu>
2024-03-18 07:10:48 -0400

    adding max_length attribute to char fields

--- socialnetwork/models.py
+++ socialnetwork/models.py
@@ -11,28 +11,28 @@ class Profile(models.Model):
         return f'id={self.id}, bio="{self.bio}" user={self.user.id}'
     
 class Playlist(models.Model):
-    id = models.CharField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=300)
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     total_mins = models.PositiveIntegerField()
-    title = models.CharField()
-    thumbnail =  models.CharField()
-    ChannelId = models.CharField()
-    UserId = models.CharField()
+    title = models.CharField(max_length=200)
+    thumbnail =  models.CharField(max_length=300)
+    ChannelId = models.CharField(max_length=70)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
 
     def __str__(self):
         return f"id={self.id}, title={self.title}"
 
 class video(models.Model):
-    id = models.CharField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=300)
     total_mins = models.IntegerField()
-    title = models.CharField()
-    thumbnail = models.CharField()
+    title = models.CharField(max_length=200)
+    thumbnail = models.CharField(max_length=300)
     # channel = models.on
 
 class Channel(models.Model):
-    id = models.CharField(primary_key=True)
-    name = models.CharField()
-    thumbnail = models.CharField()
+    id = models.CharField(primary_key=True, max_length=300)
+    name = models.CharField(max_length=70)
+    thumbnail = models.CharField(max_length=300)
 
 
 
