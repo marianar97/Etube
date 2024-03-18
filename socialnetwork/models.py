c55cbbfd50778d39805a84088aec1a21ebda7cc3
Mariana <marianar@andrew.cmu.edu>
2024-03-18 06:46:23 -0400

    adding models in models.py

--- socialnetwork/models.py
+++ socialnetwork/models.py
@@ -11,5 +11,28 @@ class Profile(models.Model):
         return f'id={self.id}, bio="{self.bio}" user={self.user.id}'
     
 class Playlist(models.Model):
+    id = models.CharField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     total_mins = models.PositiveIntegerField()
+    title = models.CharField()
+    thumbnail =  models.CharField()
+    ChannelId = models.CharField()
+    UserId = models.CharField()
+
+    def __str__(self):
+        return f"id={self.id}, title={self.title}"
+
+class video(models.Model):
+    id = models.CharField(primary_key=True)
+    total_mins = models.IntegerField()
+    title = models.CharField()
+    thumbnail = models.CharField()
+    # channel = models.on
+
+class Channel(models.Model):
+    id = models.CharField(primary_key=True)
+    name = models.CharField()
+    thumbnail = models.CharField()
+
+
+
